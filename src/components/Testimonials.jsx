c3e5c35f5574bc3c5235f0f4190fb58a501bc490
Bongani <bbongs421@gmail.com>
2023-01-28 17:11:20 +0200

    Add contents for the testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,23 @@
+import { testimonials } from "./data";
+
 const Testimonials = () => {
     return (
         <section id='testimonials'>
-            <h1>I am a Testimonial</h1>
+            <h2>Testimonials</h2>
+            <view className="testimonials">
+                {testimonials.map(item => {
+                    return (
+                        <view className="testimonial">
+                            <h5>Rating: {item.rating} <img src={require('../icons_assets/star-rating-icon.jpg')} alt='Star icon' width='20px' /></h5>
+                            <view className="profile">
+                                <img src={item.image} alt={item.name} />
+                                <p>{item.name}</p>
+                            </view>
+                            <p>{item.testimonial}</p>
+                        </view>
+                    )
+                })}
+            </view>
         </section>
     )
 }
