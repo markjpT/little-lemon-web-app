3ebfb1a80d2fe09820a13a2082f7aa64cfab8a54
Bongani <bbongs421@gmail.com>
2023-01-28 14:35:50 +0200

    create specials api

---
+++ src/components/data.jsx
@@ -0,0 +1,28 @@
+const specials = [
+    {
+        id: 1,
+        title: 'Greek salad',
+        price: '$12.99',
+        description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
+        image: require('../icons_assets/greek-salad.jpg'),
+        alt: 'Greek salad dish'
+    },
+    {
+        id: 2,
+        title: 'Bruchetta',
+        price: '$5.99',
+        description: 'Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned salt and olive oil.',
+        image: require('../icons_assets/bruchetta.jpg'),
+        alt: 'Bruchetta sandwitch'
+    },
+    {
+        id: 3,
+        title: 'Lemon Dessert',
+        price: '$5.00',
+        description: 'This comes straight from grandma\'s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
+        image: require('../icons_assets/lemon-dessert.jpg'),
+        alt: 'Lemon dessert'
+    }
+]
+
+export default specials;
