189e96a5b446f90ab51f3a8ada5d74b561aefccf
Bongani <bbongs421@gmail.com>
2023-01-28 14:36:53 +0200

    Create content for the specials section on the homepage

--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -1,4 +1,8 @@
+import React from 'react';
+import specials from './data';
+
 const Specials = () => {
+
     return (
         <section id='specials-section'>
             <view className="specials-top">
@@ -6,7 +10,23 @@ const Specials = () => {
                 <button className="button order-online">Order online</button>
             </view>
             <view className="specials">
-
+                {specials.map(item => {
+                    return (
+                        <view className='special'>
+                            <view className="special-content">
+                                <img src={item.image} alt={item.alt} />
+                                <view className="special-name">
+                                    <h4>{item.title}</h4>
+                                    <p>{item.price}</p>
+                                </view>
+                                <p>{item.description}</p>
+                            </view>
+                            <view className='order'>
+                                Order a delivery <img src={require('../icons_assets/cyclist-icon.png')} alt='Cyclist icon' width='20px' />
+                            </view>
+                        </view>
+                    )
+                })}
             </view>
         </section>
     )
