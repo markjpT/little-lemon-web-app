04649b03ca9d57895862498b77e351907c0bddca
Bongani <bbongs421@gmail.com>
2023-01-28 11:40:38 +0200

    Add a copyright in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,41 @@
+import { Link } from 'react-router-dom'
 import Logo from '../icons_assets/logo.svg'
 
 const Footer = (props)=> {
     return (
             <section id='footer'>
-                <img src={Logo} alt='Logo' />
-                <div className='doormat-navigation'>
-                    <h4>Doormat Navigation</h4>
-                    <ul>
-                        <li><a href='/home'>Home</a></li>
-                        <li><a href='/about'>About</a></li>
-                        <li><a href='/menu'>Menu</a></li>
-                        <li><a href='/reservations'>Reservations</a></li>
-                        <li><a href='/order-online'>Order Online</a></li>
-                        <li><a href='/login'>Login</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h4>Contact</h4>
-                    <p>97 Bob Junction</p>
-                    <p>+31 505 256 2654</p>
-                    <p>littlelemon@restuarant.com</p>
-                </div>
-                <div>
-                    <h4>Social Media Links</h4>
-                    <ul>
-                        <li><a href='https://www.instagram.com'>Instagram</a></li>
-                        <li><a href='https://www.twitter.com'>Twitter</a></li>
-                        <li><a href='https://www.facebook.com'>Facebook</a></li>
-                    </ul>
-                </div>
+                <view className="footer-info">
+                    <img src={Logo} alt='Logo' />
+                    <div className='doormat-navigation'>
+                        <h4>Doormat Navigation</h4>
+                        <ul>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/menu'>Menu</Link></li>
+                            <li><Link to='/reservations'>Reservations</Link></li>
+                            <li><Link to='/order-online'>Order Online</Link></li>
+                            <li><Link to='/login'>Login</Link></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4>Contact</h4>
+                        <p>97 Bob Junction</p>
+                        <p>+31 505 256 2654</p>
+                        <p>littlelemon@restuarant.com</p>
+                    </div>
+                    <div>
+                        <h4>Social Media Links</h4>
+                        <ul>
+                            <li><a href='https://www.instagram.com'>Instagram</a></li>
+                            <li><a href='https://www.twitter.com'>Twitter</a></li>
+                            <li><a href='https://www.facebook.com'>Facebook</a></li>
+                        </ul>
+                    </div>
+                </view>
+                <view className="copyright">
+                    Copyright ©️ 2023 Gelo
+                    <a href='https://www.github.com/Bongani001' target='_blank' rel="noreferrer"><img src={require('../icons_assets/github-logo.png')} alt='Github logo' style={{width: 20, borderRadius: 5}} /></a>
+                </view>
             </section>
     )
 }
