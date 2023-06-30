d3f7f917a5c4b20d87c3c16e070341114566324f
Bongani <bbongs421@gmail.com>
2023-06-30 19:02:44 +0200

    fix routes

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename='little-lemon-web-app'>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
